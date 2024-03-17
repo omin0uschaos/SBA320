584a52587c315c4032e92fd323b290360f5e8719
omin0uschaos <10238707+omin0uschaos@users.noreply.github.com>
2024-03-17 01:03:58 -0700

    add responsive navbar

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <NavBar />
     <Routes>
       <Route path='/' element={<Home />}>
-
+        
       </Route>
     </Routes>
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,30 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 import '../styles/NavBar.css'
+import Hamburger from "./Hamburger"
 
 function NavBar() {
+  const [showNav, setShowNav] = useState(false);
+
+  const toggleNav = () => {
+    setShowNav(!showNav);
+  }
+
   return (
     <nav>
         <ul>
             <li><div>LOGO</div></li>
-            <div id="navLinks">
+            
+            <div id="navLinks" className={showNav ? "show" : ""} onClick={() => setShowNav(false)}>
+              
               <li>Home</li>
               <li>About</li>
               <li>Create</li>
               <li>Contact</li>
             </div>
-            <li><div>Light/Dark</div></li>
+            <div className="menuIcon" onClick={toggleNav}>
+              <Hamburger />
+            </div>
         </ul>
 
     </nav>
